387a0e7d07532e07f4121113f790aa7ebc096030
Davis Lin <dlin@blizzard.com>
2017-05-03 13:13:06 +0800

    Test

--- part.sh
+++ part.sh
@@ -1,8 +1,12 @@
 # Install cobbler in CentOS 7.3
 
+RedHatRelease=$(cat /etc/redhat-release)
+echo $RedHatRelease
+
 if $(grep -q "SELINUX=enforcing" /etc/selinux/config); then
 	sed -in 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
-	sudo reboot
+	#sudo reboot
+	echo 'SELinux is enabled'
 else
-    echo 'Keep going';
+    echo 'Keep going'
 fi
