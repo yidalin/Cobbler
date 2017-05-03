474e9a8fe91dfd45be2394d8d61241bd20c2855e
Davis Lin <dlin@blizzard.com>
2017-05-03 13:18:51 +0800

    Adding variables for record distribution

--- part.sh
+++ part.sh
@@ -3,6 +3,12 @@
 RedHatRelease=$(cat /etc/redhat-release)
 echo $RedHatRelease
 
+Ditro=$(echo $RedHatRelease | awk -F" " '{ print $1 }')
+echo $Ditro
+
+DistroVer=$(echo $RedHatRelease | awk -F" " '{ print $4 }' | awk -F"." '{ print $1}')
+echo $DistroVer
+
 if $(grep -q "SELINUX=enforcing" /etc/selinux/config); then
 	sed -in 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 	#sudo reboot
