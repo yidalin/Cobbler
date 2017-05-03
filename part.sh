1e79610761abafd51458bb0dcc35e63f53117d29
Davis Lin <dlin@blizzard.com>
2017-05-03 13:30:04 +0800

    Re-write if loop

--- part.sh
+++ part.sh
@@ -9,17 +9,27 @@ echo $Ditro
 DistroVer=$(echo $RedHatRelease | awk -F" " '{ print $4 }' | awk -F"." '{ print $1}')
 echo $DistroVer
 
-if [ $DistroVer == '6' ]; then
-    echo 'it is 7'
+# Checking the distribution, it should be CentOS
+if [ $Distro == 'CentOS' ]; then
+    echo 'Good, the OS is CentOS!'
 else
-    echo 'it is not 7'
+    echo 'The OS is not CentOS, bye bye.'
+    exit
+fi
+
+
+# Checking the distribution version, it should be 7
+if [ $DistroVer == '7' ]; then
+    echo 'Good, the distribution version is 7.'
+else
+    echo 'The distribution version is not 7, bye bye.'
     exit
 fi
 
 if $(grep -q "SELINUX=enforcing" /etc/selinux/config); then
 	sed -in 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 	#sudo reboot
-	echo 'SELinux is enabled'
+	echo 'SELinux is enabled, and now switch to disabled.'
 else
-    echo 'Keep going'
+    echo 'SELinux is already disabled.'
 fi
