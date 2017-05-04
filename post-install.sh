aeb7fd61f8a55afcca8c2ec2247c243bf0964d9c
root <root@dev.8bitsworld.com>
2017-05-05 03:51:23 +0800

    Modify script for DHCP service

--- post-install.sh
+++ post-install.sh
@@ -1,105 +1,28 @@
-# Install cobbler in CentOS 7.3
+echo -e "\n>> Use Cobbler to manage dhcp config."
+sed -i 's/manage_dhcp: 0/manage_dhcp: 1/g' /etc/cobbler/settings
 
-RedHatRelease=$(cat /etc/redhat-release)
-#echo $RedHatRelease
+DHCPSubnet='192.168.0.0'
+DHCPNetmask='255.255.255.0'
+DHCPGateway='192.168.0.1'
+DHCP_DNS='192.168.0.100'
+DHCPReleaseStart='192.168.0.200'
+DHCPReleaseEnd='192.168.0.220'
 
-Distro=$(echo $RedHatRelease | awk -F" " '{ print $1 }')
-#echo $Distro
 
-DistroVer=$(echo $RedHatRelease | awk -F" " '{ print $4 }' | awk -F"." '{ print $1}')
-#echo $DistroVer
+sed -i "s/192.168.1.0/$DHCPSubnet/g" dhcp.template
+sed -i "s/255.255.255.0/$DHCPNetmask/g" dhcp.template
+sed -i "s/192.168.1.5/$DHCPGateway/g" dhcp.template
+sed -i "s/192.168.1.1/$DHCP_DNS/g" dhcp.template
+sed -i "s/192.168.1.100/$DHCPReleaseStart/g" dhcp.template
+sed -i "s/192.168.1.254/$DHCPReleaseEnd/g" dhcp.template
 
-CobblerServer=$(hostname -I)
-TFTPServer=$(hostname -I)
+exit
 
-OldCobblerPassword=$(grep default_password_crypted /etc/cobbler/settings | awk '{ print$2 }')
-NewCobblerPassword=$(openssl passwd -1 -salt 'salt' 'cobbler')
+echo -e "\nSyncing Cobbler settings."
+cobbler sync 
 
-# Checking the distribution, it should be CentOS
-if [ "$Distro" == 'CentOS' ]; then
-    echo -e "\n>> Good, the OS is CentOS!"
-else
-    echo -e "\n>> The OS is not CentOS, bye bye."
-    exit
-fi
+echo -e "\nRestarting dhcp service..."
+systemctl restart dhcpd.service
 
-# Checking the distribution version, it should be 7
-if [ "$DistroVer" == '7' ]; then
-    echo -e "\n>> Good, the distribution version is 7."
-else
-    echo -e "\n>> The distribution version is not 7, bye bye."
-    exit
-fi
-
-# Checking the SELinux setting
-if $(grep -q "SELINUX=enforcing" /etc/selinux/config); then
-    sed -in 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
-    echo -e "\n>> SELinux is enabled, and now switch to disabled, the OS will reboot in 30 second."
-    sleep 10
-    sudo reboot
-else
-    echo -e "\n>> SELinux is already disabled."
-fi
-
-# Checking the network can reach to internet
-
-ping -c 2 8.8.8.8 > /dev/null
-
-if [ $? != '0' ]; then
-    echo -e "\n>> Please make sure your network is workable."
-    exit
-else
-    echo -e "\n>> OK, keep going."
-fi
-
-echo -e "\n>> Installing repository: epel-release"
-#sleep 3
-yum install -y epel-release
-
-echo -e "\n>> Installing Cobbler package and its dependency packages"
-#sleep 3
-yum install -y cobbler cobbler-web dhcp xinetd tftp-server python-ctypes pykickstart fence-agents
-
-echo -e "\n>> Enable and start Cobbler service and Apache service"
-systemctl enable cobblerd.service
-systemctl enable httpd.service
-systemctl start cobblerd.service
-systemctl start httpd.service
-
-echo -e "\n>> Run command: cobbler get-loaders"
-cobbler get-loaders
-
-echo -e "\n>> Replacing some parameter of cobbler setting."
-sed -i "s/server: 127.0.0.1/server: $CobblerServer/g" /etc/cobbler/settings
-sed -i "s/next_server: 127.0.0.1/next_server: $TFTPServer/g" /etc/cobbler/settings
-
-echo -e "\n>> Turn on the TFTP service."
-grep -l 'disable' /etc/xinetd.d/tftp | xargs -i sed -i 's/yes/no/g' {}
-
-echo -e "\n>> Enable rsync and xinetd, also start them."
-systemctl enable rsyncd.service
-systemctl enable xinetd.service
-systemctl start rsyncd.service
-systemctl start xinetd.service
-
-echo -e "\n>> Installing debmirror for install Debian OS"
-yum install -y debmirror
-
-echo -e "\n>> Changing /etc/debmirror.conf (Only needed when install Debian OS)"
-sed -i 's/@dists="sid";/#@dists="sid";/g' /etc/debmirror.conf
-sed -i 's/@arches="i386";/#@arches="i386";/g' /etc/debmirror.conf
-
-echo -e "\n>> Replacing the default root password"
-sed -i "s|$OldCobblerPassword|$NewCobblerPassword|g" /etc/cobbler/settings
-
-echo -e "\n>> Restarting Cobbler service..."
+echo -e "\nRestarting Cobbler service..."
 systemctl restart cobblerd.service
-sleep 1
-
-echo -e "\n>> Check the prerequisites..."
-CobblerCheck=$(cobbler check | tee /dev/tty)
-
-if [ "$CobblerCheck" != 'No configuration problems found.  All systems go.' ]; then
-    echo -e "\n>> Please check the prerequisites of Cobbler"
-    exit
-fi
