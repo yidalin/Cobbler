71dcd0a6805954f3efbd3efa1de49cddc1afcd64
root <root@cobbler.8bitsworld.com>
2017-05-12 03:28:01 +0800

    Change something

--- post-install.sh
+++ post-install.sh
@@ -1,13 +1,13 @@
 echo -e "\n>> Use Cobbler to manage dhcp config."
 sed -i 's/manage_dhcp: 0/manage_dhcp: 1/g' /etc/cobbler/settings
 
-DHCPSubnet='192.168.0.0'
+DHCPSubnet='172.16.0.0'
 DHCPNetmask='255.255.255.0'
-DHCPGateway='192.168.0.1'
-DHCP_DNS='192.168.0.100'
-DHCPReleaseStart='192.168.0.200'
-DHCPReleaseEnd='192.168.0.220'
-
+DHCPGateway='172.16.0.1'
+DHCP_DNS='172.16.0.100'
+DHCPReleaseStart='172.16.0.200'
+DHCPReleaseEnd='172.16.0.220'
+DHCPListenInterface="ens19"
 
 sed -i "s/RELEASE_SUBNET/$DHCPSubnet/g" dhcp.template
 sed -i "s/SUBNET_MASK/$DHCPNetmask/g" dhcp.template
@@ -16,10 +16,10 @@ sed -i "s/DNS/$DHCP_DNS/g" dhcp.template
 sed -i "s/RELEASE_START/$DHCPReleaseStart/g" dhcp.template
 sed -i "s/RELEASE_END/$DHCPReleaseEnd/g" dhcp.template
 
-echo -e ">> \nChanging some setting within the dhcp.template"
+echo -e "\n>> Changing some setting within the dhcp.template"
 mv -f /etc/cobbler/dhcp.template /etc/cobbler/dhcp.template.bk
 
-echo -e "\n>>Replacing the /etc/cobbler/dhcp.tempate with the dhcp.template"
+echo -e "\n>> Replacing the /etc/cobbler/dhcp.tempate with the dhcp.template"
 cp  ./dhcp.template /etc/cobbler/dhcp.template
 
 echo -e "\n>> Restarting Cobbler service..."
@@ -29,6 +29,9 @@ sleep 2
 
 echo -e "\n>> Syncing Cobbler settings."
 cobbler sync 
+
+echo -e "\n>> Change listen interface"
+echo "DHCPDARGS=\"$DHCPListenInterface\";" >> /etc/sysconfig/dhcpd
 
 echo -e "\n>> Restarting dhcp service..."
 systemctl restart dhcpd.service
