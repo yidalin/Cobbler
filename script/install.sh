a152cc94805ab7cab890b4eb0b2b06843c570bb8
Davis Lin <da860610@gmail.com>
2017-11-15 22:22:32 +0800

    Alpha test

--- script/install.sh
+++ script/install.sh
@@ -4,6 +4,9 @@
 cobbler_server='172.16.0.103'
 tftp_server='172.16.0.103'
 selinux_mode='permissive'
+root_password_old=$(grep default_password_crypted /etc/cobbler/settings | awk '{ print$2 }')
+root_password_new=$(openssl passwd -1 -salt 'salt' 'cobbler')
+cobbler_check=$(cobbler check | tee /dev/tty)
 
 # Checking the SELinux setting
 echo -e ">> Checking the SELinux setting"
@@ -15,6 +18,8 @@ else
     echo ">> SELinux mode: $(getenforce)" 
 fi
 
+# setsebool -P httpd_can_network_connect true
+
 # Installing the EPEL repo
 echo -e "\n>> Installing repository: epel-release"
 #https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
@@ -24,22 +29,8 @@ yum install -y epel-release
 echo -e "\n>> Installing Cobbler package and its dependency packages"
 yum install -y cobbler cobbler-web httpd rsync tftp-server xinetd dhcp python-ctypes debmirror pykickstart cman fence-agents dnsmasq
 
-# Enable and start the Cobbler service and Apache service 
-echo -e "\n>> Enable and start Cobbler service and Apache service"
-systemctl enable cobblerd.service
-systemctl start cobblerd.service
-
-systemctl enable httpd.service
-systemctl start httpd.service
-
-exit
-# Cobbler get loaders
-echo -e "\n>> Run command: cobbler get-loaders"
-cobbler get-loaders
-
 # Replacing some variables or parameters of the Cobbler setting file
 echo -e "\n>> Replacing some parameter of cobbler setting."
-
 echo -e ">> Set the listening IP to $cobbler_server"
 sed -i "s/server: 127.0.0.1/server: $cobbler_server/g" /etc/cobbler/settings
 
@@ -58,15 +49,6 @@ sed -i 's/manage_dhcp: 0/manage_dhcp: 1/g' /etc/cobbler/settings
 echo -e "\n>> Turn on the TFTP service."
 grep -l 'disable' /etc/xinetd.d/tftp | xargs -i sed -i 's/yes/no/g' {}
 
-# Enable and start rsync and xineted
-echo -e "\n>> Enable rsync and xinetd, also start them."
-systemctl enable rsyncd.service
-systemctl enable xinetd.service
-systemctl start rsyncd.service
-systemctl start xinetd.service
-
-exit
-
 echo -e "\n>> Installing debmirror for install Debian OS"
 yum install -y debmirror
 
@@ -74,10 +56,20 @@ echo -e "\n>> Changing /etc/debmirror.conf (Only needed when install Debian OS)"
 sed -i 's/@dists="sid";/#@dists="sid";/g' /etc/debmirror.conf
 sed -i 's/@arches="i386";/#@arches="i386";/g' /etc/debmirror.conf
 
-OldCobblerPassword=$(grep default_password_crypted /etc/cobbler/settings | awk '{ print$2 }')
-NewCobblerPassword=$(openssl passwd -1 -salt 'salt' 'cobbler')
 echo -e "\n>> Replacing the default root password"
-sed -i "s|$OldCobblerPassword|$NewCobblerPassword|g" /etc/cobbler/settings
+sed -i "s|$root_password_old|$root_password_new|g" /etc/cobbler/settings
+
+# Enable and start rsync and xineted
+echo -e "\n>> Enable rsync and xinetd, also start them."
+systemctl enable rsyncd.service
+systemctl enable xinetd.service
+systemctl enable httpd.service
+systemctl enable cobblerd.service
+
+systemctl start rsyncd.service
+systemctl start xinetd.service
+systemctl start httpd.service
+systemctl start cobblerd.service
 
 echo -e "\n>> Get the loaders again"
 cobbler get-loaders
@@ -89,9 +81,7 @@ echo -e "\n>> Syncing Cobbler settings"
 cobbler sync
 
 echo -e "\n>> Check the prerequisites..."
-CobblerCheck=$(cobbler check | tee /dev/tty)
-
-if [ "$CobblerCheck" != 'No configuration problems found.  All systems go.' ]; then
+if [ "$cobbler_check" != 'No configuration problems found.  All systems go.' ]; then
     echo -e "\n>> Please check the prerequisites of Cobbler"
     exit
 fi
