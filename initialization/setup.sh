dc5d9a2912066c36b37e1e1e8ff25bf13542af66
Davis Lin <da860610@gmail.com>
2018-01-02 01:49:00 +0800

    alpha test

---
+++ initialization/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -f ./bashrc ]; then
+    echo "The bashrc file not exist!"
+    exit
+fi
+
+echo "Backup the ~/.bashrc to ~/.bashrc.bk"
+mv ~/.bashrc ~/.bashrc.$(date +%Y%m%d-%H%M%S)
+echo "Copy the new .bashrc to ~/.bashrc"
+cp -a ./bashrc ~/.bashrc
+
+ls -al ~ | grep ".bashrc"
